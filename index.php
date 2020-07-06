d8d35b2fce5ded6c58f4919e2da5bf780b9dd919
prashanth <prashanth1krishnamurthy@gmail.com>
2020-07-06 21:50:35 +0530

    cool modifications with canvas

--- index.php
+++ index.php
@@ -4,74 +4,89 @@
       <link rel="stylesheet" href="https://unpkg.com/mvp.css">
       <link rel="stylesheet" href="./assets/styles.css">
       <link rel="manifest" href="./manifest.webmanifest">
+      <script>
+        window.onload = function(e){ 
+            initCamera();
+        }
+      </script>
   </head>
+
   <body style="text-align:center;">
 
-  
     <?php
-
     class claimImg {
         /**
          * Retrieve data from database
          */
         public function select() {
-            include_once 'db.php';
-            
+            include_once 'db.php';  
             if($db != null) {
                 $result=$db->query("SELECT * FROM claim_images");
-            
                 $claimData=array();
                 while($row=$result->fetchArray(SQLITE3_ASSOC)) {
                     array_push($claimData, array("claim_number"=>$row['claim_number'], "image_url"=>$row['image_url']));
-                }
-                
+                }  
                 // echo var_dump($claimData); 
+                // print(json_encode(array_reverse($claimData))); // useful if PHP is called by ext. - retain
                 
-                // this is useful if PHP is called by ext. - retain
-                // print(json_encode(array_reverse($claimData)));
                 return $claimData;
                 $db->close();
-
             }
             else {
                 print(json_encode(array("Error: Cannot connect to DB")));
             }
-        }
+        } // select()
+    } // claimImg class
 
-        
-
-    }
-
-    
-
+    // initiate class and invoke function
     $claimImg=new claimImg();
     $claimData = $claimImg->select();
 
-    if(isset($_GET["upload"])) {
-        $info = $_GET["upload"];
-    }
-    else {
-        $info = "";
-    }
-
-    //end
+    // dynamically display upload message 
+    // this page will be reloaded when form is submitted - modify for Ajax
+    if(isset($_GET["upload"])) $info = $_GET["upload"];
+    else $info = ""; 
+    //end of main PHP
     ?>
+
+    <!-- 
+     * This is the actual HTML
+    -->
 
     <h2>
         PHP + PWA Demo
     </h2>
     <form action="upload.php" method="post" enctype="multipart/form-data" style="text-align:center;">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
 
-        Claim Number:
+        <label for="claimNumber">Claim Number</label>
         <input type="text" name="claimNumber" id="claimNumber">
+           
+        <div id="camerasupport">
+            <label for="video">Capture Image</label>
+            <video id="video" name="video"></video> 
+            <div>
+                <button id="startbutton" onclick="takepicture(event)">Take photo</button>
+            </div>
+
+            <label for="photos">Your Images</label>
+            <div id="photos" name="photos" class="photos"></div>
+            <input type="hidden" id="photosData" name="photosData">
+            <canvas id="canvas" style="display:none;"></canvas>
+        </div>
+
+        <div id="nocamerasupport">
+            <label for="fileToUpload">Select image to upload</label>
+            <input type="file" name="fileToUpload[]" id="fileToUpload" accept="image/*" multiple> 
+        </div>
 
         <input type="submit" value="Upload Image" name="submit">
         <span class="info"><?php echo $info; ?></span>
+
+        <input type="hidden" id="cameraCheck" name="cameraCheck" value="false">
     </form>
 
     <div style="text-align:center;">
+        <!-- display uploaded images. Modify for actual use -->
         <?php foreach( $claimData as $key => $value ): ?>
                 <h4 class="margin-top:5em;"> <?= $value['claim_number'] ?></h4>
                 <img src="<?= $value['image_url'] ?>" class="claim-img"/>
@@ -80,16 +95,146 @@
     </div>
 
     <script>
+        var width = 320; 
+        var height = 0; 
+
+        var video = null;
+        var canvas = null;
+        var startbutton = null;
+        var streaming = false;
+
+        cameraSupport = false;
+
         if ('serviceWorker' in navigator) {
+            // register service worker (PWA)
             window.addEventListener('load', function() {
                 navigator.serviceWorker.register('./sw.js').then(function(registration) {
                     // Registration was successful
                     console.log('Service Worker registration successful with scope: ', registration.scope);
-                }, function(err) {
+
+                }, 
+                function(err) {
                     // registration failed :(
                     console.log('ServiceWorker registration failed: ', err);
                 });
             });
+        }
+
+        
+        function initCamera() {
+            video = document.getElementById('video');
+            canvas = document.getElementById('canvas');
+            startbutton = document.getElementById('startbutton');
+
+            clearphoto();
+
+            navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+                
+                cameraSupport = true;
+                document.getElementById("cameraCheck").value = true;
+                document.getElementById("nocamerasupport").style.display = "none";
+            })
+            .catch(function(err) {
+                cameraSupport = false;
+                console.log("Taking images is not supported on your device. " + err);
+                document.getElementById("camerasupport").style.display = "none";
+            });
+            
+            video.addEventListener('canplay', function(ev){
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth/width);
+                    video.setAttribute('width', width);
+                    video.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                    streaming = true;
+                }
+            }, false);
+        }
+
+        function clearphoto() {
+            var context = canvas.getContext('2d');
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            // var data = canvas.toDataURL('image/png');
+            // photo.setAttribute('src', data);
+
+            document.getElementById('photos').innerHTML = "";
+            //document.getElementById("photosData").innerHTML = "";
+        }
+
+        function takepicture(event) {
+            try {
+                var context = canvas.getContext('2d');
+               
+                if (width && height) {
+                    canvas.width = width;
+                    canvas.height = height;
+                    context.drawImage(video, 0, 0, width, height);
+                   
+                    var data = canvas.toDataURL('image/jpeg', 0.5);
+
+                    document.getElementById("photosData").value = document.getElementById("photosData").value  + "<#>" + data;
+                    
+                    // const photos = document.getElementById('photos');  
+                    const photoDyn = document.createElement('img'); 
+                    photoDyn.src = data;
+                    photoDyn.id = getUniqueId();
+                    const subText = document.createElement('div');
+
+                    const photoDynContainer = document.createElement('div');
+                    photoDynContainer.id = `con${photoDyn.id}`;
+                    photoDynContainer.appendChild(photoDyn);
+
+                    subText.innerHTML = `<button onclick="removePhoto('` + photoDynContainer.id + `')"> Delete </button>`;
+                    subText.className = "imageSubText";
+
+                    photoDynContainer.appendChild(subText);
+                    
+                    document.getElementById('photos').appendChild(photoDynContainer); 
+            
+                } else clearphoto();
+            }
+            catch(e) {
+                console.error(e);
+            }
+            finally{
+                event.stopPropagation();
+                event.preventDefault();
+            }
+        }
+        
+        function getUniqueId() {
+            const chars =
+            '1234567890' +
+            'abcdefghijklmnopqrstuvwxyz' +
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+            const charsLen = chars.length; // 62
+
+            const id = []
+            for (let i = 0; i < 10; i++) {
+                id.push(chars[(Math.round(Math.random() * 100)) % charsLen]);
+            }
+            return id.join('');
+            
+        }
+
+        function removePhoto(id) {
+            try {
+                document.getElementById('photos').removeChild(document.getElementById(id));
+            }
+            catch(e) {
+            
+            }
+            finally {
+                event.stopPropagation();
+                event.preventDefault();
+            }
         }
     </script>
   </body>
--- upload.php
+++ upload.php
@@ -1,66 +1,111 @@
 <?php
 $target_dir = "images/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $claimNumber = $_POST['claimNumber'];
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$cameraCheck = $_POST['cameraCheck'];
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "✅ File is an image - " . $check["mime"] . ".<br>";
-    $uploadOk = 1;
-  } else {
-    echo "👎🏻 File is not an image.";
-    $uploadOk = 0;
-  }
 
-  if($claimNumber == "") {
-    echo "👎🏻 Claim Number is required.";
-    $uploadOk = 0;
-  }
-}
+if ($cameraCheck == "false") {
+  // fair warning: messy code ahead
+  $uploadOk = 1;
+  
+  $i=0;
+  $total = count($_FILES['fileToUpload']['name']);
+  
+  for ($i=0 ; $i < $total ; $i++ ) {
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  
+    echo "<br><br>"."Processing file:" . $target_file . "<br>";
+    echo "------------------" . "<br>";
+    // Check if image file is a actual image or fake image  
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"][$i]);
+      if($check !== false) {
+        echo "✅ File is an image - " . $check["mime"] . ".<br>";
+        $uploadOk = 1;
+      } else {
+        echo "👎🏻 File is not an image.";
+        $uploadOk = 0;
+      }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "👎🏻 File already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 1000000) {
-  echo "👎🏻 Your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "👎🏻 Only JPG, JPEG, PNG & GIF files are allowed.<br>";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "<br><br>Your file was not uploaded. Fix errors and retry.";
-  // header("Location:/server?upload=Sorry, your file was not uploaded.");
-  // if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-
-    include_once 'db.php';
-    $fileURL = "https://$_SERVER[HTTP_HOST]/images/" . $_FILES["fileToUpload"]["name"];
-    $insert = $db->query("INSERT INTO claim_images (claim_number, image_url) VALUES ('".$claimNumber."','".$fileURL."')");
-    if(!$insert) {
-        echo $db->lastErrorMsg() . "<br>";
+      if($claimNumber == "") {
+        echo "👎🏻 Claim Number is required.";
+        $uploadOk = 0;
+      }
     }
-    $db->close();
 
-    header("Location:/?upload=File uploaded.");
-  } else {
-    echo "Sorry, there was an error uploading your file.";
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      echo "👎🏻 File already exists.";
+      $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"][$i] > 1000000) {
+      echo "👎🏻 Your file is too large.";
+      $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+      echo "👎🏻 Only JPG, JPEG, PNG & GIF files are allowed.<br>";
+      $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "<br><br>Your file was not uploaded. Fix errors and retry.";
+      // header("Location:/server?upload=Sorry, your file was not uploaded.");
+    } else {
+      // everything is ok, try to upload file
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
+
+        include_once 'db.php';
+        $fileURL = "https://$_SERVER[HTTP_HOST]/images/" . $_FILES["fileToUpload"]["name"][$i];
+        $insert = $db->query("INSERT INTO claim_images (claim_number, image_url) VALUES ('".$claimNumber."','".$fileURL."')");
+        if(!$insert) {
+            echo $db->lastErrorMsg() . "<br>";
+        }
+        $db->close();
+
+        header("Location:/?upload=File uploaded.");
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+    }
+  } // for
+} // if ($cameraCheck == false)
+else {
+  // camera supported
+  $photos = $_POST["photosData"];
+  $photosArr = explode("<#>", $_POST['photosData']); 
+
+  $arrCount = count($photosArr);
+  if ($claimNumber == "" || $arrCount < 2) {
+    header("Location:/?upload=Claim Id and images are required. Fix problems and retry.");
+    exit("error");
   }
+  else {
+    $i = 0;
+    include_once 'db.php';
+    for ($i=1; $i < $arrCount; $i++) {
+
+      $img = str_replace('data:image/jpeg;base64,', '', $photosArr[$i]);
+      $img = str_replace(' ', '+', $img);
+      $image_base64 = base64_decode($img);
+      $file = $target_dir . "image" . uniqid() . ".jpg";
+      $fileURL = "https://$_SERVER[HTTP_HOST]/" . $file;
+
+      file_put_contents($file, $image_base64);
+      $insert = $db->query("INSERT INTO claim_images (claim_number, image_url) VALUES ('".$claimNumber."','".$fileURL."')");
+      if(!$insert) {
+          echo $db->lastErrorMsg() . "<br>";
+      }
+    } // for
+    $db->close();
+    header("Location:/?upload=Images uploaded.");
+  } // else for claimNumber
 }
 ?>
